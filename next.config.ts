5469c99f21f1e87d1a20e54d8750bfccf746ec7c
Adriano Rafaelo Lumbantoruan <zalumban@dfiretail.co.id>
2026-05-30 13:26:26 +0700

    webpack

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
@@ -13,6 +5,12 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
 })
 
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-})
+
+  // ✅ FIX turbopack conflict
+  turbopack: {},
+}
+
+module.exports = withPWA(nextConfig)
